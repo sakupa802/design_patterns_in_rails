8e6d608ad71e42583bfe6e7b9e607e0f4dbf6e3f
akazaki.hiroki <akazaki.hiroki@a-tm.co.jp>
2020-03-19 04:12:54 +0900

    add abstract method

--- app/models/factory_method/CarFactory.rb
+++ app/models/factory_method/CarFactory.rb
@@ -1,5 +1,15 @@
 module FactoryMethod
   class CarFactory
+    # abstract method
+    def start
+      raise 'Called abstract method: start'
+    end
+
+    # abstract method
+    def stop
+      raise 'Called abstract method: stop'
+    end
+
     def simulate
       @car = create_car
       @car.start
